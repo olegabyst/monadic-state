1a8237c1907425430f85afd03b76e3d4cb6b2b5f
olegabyst <oleg.tsyba.ua@gmail.com>
2017-07-28 20:21:27 +0300

    prettify files

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const assert = require("assert");
 module.exports = function State(st) {
   let state = st;
   const fns = [];
-  let run = false;;
+  let run = false;
 
   function read() {
     return state;
@@ -13,41 +13,41 @@ module.exports = function State(st) {
     state = val;
   }
 
-  this.then = function (fn) {
+  this.then = function(fn) {
     if (run) {
       throw new Error("State is already run");
     }
     if (typeof fn !== "function") {
-      throw new TypeError( "You should pass a function");
+      throw new TypeError("You should pass a function");
     }
     if (fn.length < 3) {
-      throw new TypeError("Function should receive at least 3 arguments")
+      throw new TypeError("Function should receive at least 3 arguments");
     }
     fns.push(fn);
     return this;
-  }
+  };
 
-  this.run = function () {
+  this.run = function() {
     let promise = Promise.resolve();
     run = true;
 
     for (const fn of fns) {
       if (fn.length === 3) {
         promise = promise.then(val => {
-          return fn(val, read, write)
-        })
+          return fn(val, read, write);
+        });
       } else if (fn.length === 4) {
         promise = promise.then(val => {
           return new Promise((resolve, reject) => {
             fn(val, read, write, (err, val) => {
               if (err) reject(err);
               else resolve(val);
-            })
+            });
           });
         });
       }
     }
 
     return promise;
-  }
-}
+  };
+};
