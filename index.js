2946fcd739b36bbc39d7fb1a5efb4a34a2c1547b
Oleg Tsyba <oleg.tsyba.ua@gmail.com>
2017-08-01 19:11:31 +0300

    feat: make State to be Promise itself

--- index.js
+++ index.js
@@ -1,53 +1,29 @@
-const assert = require("assert");
+const state = Symbol("state");
+const root = global || window;
 
-module.exports = function State(st) {
-  let state = st;
-  const fns = [];
-  let run = false;
+const Promise = (root.Promise = root.Promise || require("bluebird"));
 
-  function read() {
-    return state;
-  }
+process.on("unhandledRejection", e => console.log("\n==== Error ====\n\n", e));
 
-  function write(val) {
+function State(initState) {
+  let state = initState;
+  const read = () => state;
+  const write = val => {
     state = val;
-  }
-
-  this.then = function(fn) {
-    if (run) {
-      throw new Error("State is already run");
-    }
-    if (typeof fn !== "function") {
-      throw new TypeError("You should pass a function");
-    }
-    if (fn.length < 3) {
-      throw new TypeError("Function should receive at least 3 arguments");
-    }
-    fns.push(fn);
-    return this;
   };
 
-  this.run = function() {
-    let promise = Promise.resolve();
-    run = true;
-
-    for (const fn of fns) {
-      if (fn.length === 3) {
-        promise = promise.then(val => {
-          return fn(val, read, write);
-        });
-      } else if (fn.length === 4) {
-        promise = promise.then(val => {
-          return new Promise((resolve, reject) => {
-            fn(val, read, write, (err, val) => {
-              if (err) reject(err);
-              else resolve(val);
-            });
-          });
-        });
+  class State extends Promise {
+    then(fn) {
+      if (typeof fn !== "function") return this;
+      if (fn.length <= 1) {
+        return super.then(fn);
+      } else {
+        return super.then(fn.bind(root, read, write));
       }
     }
+  }
 
-    return promise;
-  };
-};
+  return State.resolve(initState);
+}
+
+module.exports = State;
